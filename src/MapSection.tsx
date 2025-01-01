3eb560672a0c179fddfb64ce27e51cbc7920a66c
ThatDot <justinsanveloso@gmail.com>
2025-01-02 03:09:41 +0800

    feat: added hoverable map

--- src/MapSection.tsx
+++ src/MapSection.tsx
@@ -1,14 +1,27 @@
+import MapSVG from './MapSVG.tsx'
+import { useState } from 'react'
+
 export default function MapSection(){
+	const [municipality, setMunicipality] = useState(null);
+
+	const getMunicipality = (e) => {
+		setMunicipality(e.target.parentElement.id)
+	}
+
+	const noMunicipality = () => {
+		setMunicipality(null)
+	}
+
 	return (
 		<div id='map'>
 		  <h2>Bukidnon Today</h2>
 		  <p>The province now composes of 21 Municipalities</p>
     		  <div className='sub-container'>
 		    <div className='sub-container-left'>
-		      <h3>(Municipality Name)</h3>
+		      {municipality ? <h2>{municipality}</h2> : <h2>(Hover over Map)</h2>}
 		    </div>
 		    <div className='sub-container-right'>
-		      <h3>(Map of Bukidnon)</h3>
+		      <MapSVG handleHover={getMunicipality} leaveHover={noMunicipality}/>
 	            </div>
 		  </div>
                   <h5>...</h5>
