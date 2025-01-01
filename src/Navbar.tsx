7fd6aeaa73f05b55b4aa6b4dbeb89f181e513592
ThatDot <justinsanveloso@gmail.com>
2025-01-01 18:02:10 +0800

    feat: added navbar component

---
+++ src/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function NavBar(){
+	return (
+	  <ul className='Nav'>
+	    <img alt='logo'>
+	    <li>Intro</li>
+	    <li>Culture</li>
+	    <li>Map</li>
+	    <li>Issues</li>
+	  </ul>
+	)
+}
