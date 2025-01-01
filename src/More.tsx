8873ebcd31993ef26e87d1bb5c4c9855bc44a213
ThatDot <justinsanveloso@gmail.com>
2025-01-02 03:16:03 +0800

    fix: individualize dropdown buttons

--- src/More.tsx
+++ src/More.tsx
@@ -1,35 +1,35 @@
 import { useState } from 'react'
 
 export default function More() {
-  const [isShown, setIsShown] = useState<Boolean>(false);
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
-  const handleOnClick = () => {
-	setIsShown(!isShown)
+  const handleOnClick = (index: number) => {
+    setActiveIndex(activeIndex === index ? null : index);
   }
 
   return (
     <div id="more">
-	<h2>Learn More About Bukidnon</h2>
-	<div className='dropdown-menu'>
-	  <div className='dropdown-button' onClick={handleOnClick}><h4>Natural Wonders and Cultural Treasures</h4></div>
-	    {isShown && 
-	    <div className='dropdown-content'>
-	      <h3>Header</h3>
-	      <p>Para</p>
-	    </div>} 
-	  <div className='dropdown-button'><h4>Challenges and Hopes for the Future</h4></div>
-	    {isShown && 
-	    <div className='dropdown-content'>
-	      <h3>Header</h3>
-	      <p>Para</p>
-	    </div>} 
-	  <div className='dropdown-button'><h4>Did You Know? (Interesting Facts)</h4></div>
-	    {isShown && 
-	    <div className='dropdown-content'>
-	      <h3>Header</h3>
-	      <p>Para</p>
-	    </div>} 
-	</div>
+        <h2>Learn More About Bukidnon</h2>
+        <div className='dropdown-menu'>
+          <div className='dropdown-button' onClick={() => handleOnClick(0)}><h4>Natural Wonders and Cultural Treasures</h4></div>
+            {activeIndex === 0 &&
+            <div className='dropdown-content'>
+              <h3>Header</h3>
+              <p>Para</p>
+            </div>}
+          <div className='dropdown-button' onClick={() => handleOnClick(1)}><h4>Challenges and Hopes for the Future</h4></div>
+            {activeIndex === 1 &&
+            <div className='dropdown-content'>
+              <h3>Header</h3>
+              <p>Para</p>
+            </div>}
+          <div className='dropdown-button' onClick={() => handleOnClick(2)}><h4>Did You Know? (Interesting Facts)</h4></div>
+            {activeIndex === 2 &&
+            <div className='dropdown-content'>
+              <h3>Header</h3>
+              <p>Para</p>
+            </div>}
+        </div>
     </div>
   );
 }
