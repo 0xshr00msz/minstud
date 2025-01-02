838ab3f0a6f668dac85b5f162c4dcf80f09ec8be
ThatDot <justinsanveloso@gmail.com>
2025-01-02 16:21:21 +0800

    feat: added loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import './App.css'
+import {useState, useEffect} from 'react'
 import NavBar from './Navbar.tsx'
 import Intro from './Intro.tsx'
 import Culture from './Culture.tsx'
 import MapSection from './MapSection.tsx'
 import More from './More.tsx'
-
+import Loading from './Loading.tsx'
 
 function App() {
-
+	const [loading, setLoading] = useState<Boolean>(true);
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 2000)
+	}, [])
   return (
 	<>
+	  {loading && <Loading/>}
 	  <NavBar/>
+	  {!loading &&
 	  <div className='main-container'>   
 	    <Intro/>
 	    <Culture/>
             <MapSection/>
 	    <More/>
-	  </div>
+	  </div>}
   	</>
     )
 }
